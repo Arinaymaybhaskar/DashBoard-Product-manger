a992f7490494fa08d121810a8925113c6affcc97
Arinaymay Bhaskar <arinaymay.bhaskar.7500@gmail.com>
2023-07-10 17:25:09 +0530

    created react app

---
+++ assignment/src/App.js
@@ -0,0 +1,10 @@
+export default function App() {
+  return (
+    <div className="bg-red-200 w-screen h-screen">
+
+    <h1 className="w-full h-full flex justify-center items-center">
+      Hello world!
+    </h1>
+    </div>
+  )
+}
